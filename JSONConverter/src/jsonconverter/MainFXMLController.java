5fb06234742cc2031d5b94217598e64592d52659
Tomasz Pleśniak <31666201+Tomke997@users.noreply.github.com>
2018-04-25 12:57:55 +0200

    first

--- JSONConverter/src/jsonconverter/MainFXMLController.java
+++ JSONConverter/src/jsonconverter/MainFXMLController.java
@@ -5,8 +5,15 @@
  */
 package jsonconverter;
 
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,8 +30,24 @@ public class MainFXMLController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        try {
+                    String hostname="Unknown";
+                    String username =System.getProperty("user.name");
+                    
+                    TimeZone tz =  TimeZone.getTimeZone("Europe/Copenhagen");
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    sdf.setTimeZone(tz);
+                    
+                    InetAddress addr;
+                    addr = InetAddress.getLocalHost();
+                    hostname =addr.getHostName();
+                    
+                    System.out.println("host name : "+ hostname);
+                    System.out.println("user name : "+ username);
+                    System.out.println("time : "+ sdf.format(new Date()));
+                            } catch (UnknownHostException ex) {
+                    Logger.getLogger(MainFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+                }
     }
     
     @Override
