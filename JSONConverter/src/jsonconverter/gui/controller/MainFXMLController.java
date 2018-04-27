d6ea4ceccd98d3f9cd234b901da80464e5c96acc
MapeSVK <michal.moravik@icloud.com>
2018-04-27 12:32:40 +0200

    filechooser

--- JSONConverter/src/jsonconverter/gui/controller/MainFXMLController.java
+++ JSONConverter/src/jsonconverter/gui/controller/MainFXMLController.java
@@ -7,6 +7,7 @@ package jsonconverter.GUI.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -20,6 +21,8 @@ public class MainFXMLController implements Initializable {
 
     @FXML
     private Button btn;
+    @FXML
+    private Button importFileButton;
 
     /**
      * Initializes the controller class.
@@ -28,5 +31,23 @@ public class MainFXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void importFileButtonClick(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+            FileChooser.ExtensionFilter filtermP4 = new FileChooser.ExtensionFilter("select mp4","*.mp4");
+            FileChooser.ExtensionFilter filterMpeg4 = new FileChooser.ExtensionFilter("select mpeg4","*.mpeg4");
+            fileChooser.getExtensionFilters().addAll(filtermP4,filterMpeg4);
+            File file = fileChooser.showOpenDialog(null);
+            
+            if (file!=null){ //if statement only to avoid nullPointException after pressing "cancel" in filechooser
+            String filePath = file.toString();
+            FileTextField.setText(filePath); //insert path of the file into the textField
+            }
+            else {
+                System.out.println("File was not choosen.");
+            }              
+    }
+    
     
 }
